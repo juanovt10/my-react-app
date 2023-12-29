13b34201bdbaf5331cfaf12867f11f1e485cf945
Juan Velasquez <juanovelasquez10@gmail.com>
2023-12-29 14:44:20 +0000

    Install ES7 snippets and add the file that explains them

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,65 @@
+// this is created by typing rfce
+// 1. creates a standard stateless component 
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// ---------------------------------
+
+// this is created using racfe and then tab
+// 2. this create a stateless component with arrow function 
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// -----------------------------------
+
+// this is created with rce and then tab
+// 3. creates a stateful component
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// ----------------------------------------
+
+// this is created by rconst
+// 4. it creates the constructor stuff for stateful components
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// ---------------------------------------------
+
+// this is created using clg
+// 5. creates a console.log 
+// console.log(first)
+
+// ------------------------------------
+
+// this is created using clo
+// 6. creates a console log that provides a title before the print
+// console.log('first', first)
