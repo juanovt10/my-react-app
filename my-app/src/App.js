7f48e42ba27cd87053f2740dc7739b96d31e0410
Juan Velasquez <juanovelasquez10@gmail.com>
2023-12-29 12:37:23 +0000

    Added sidebar challenge and initial statful component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import Sidebar from './components/SidebarChallenge';
+import StatefulGreeting from './components/StatefulGreeting';
+
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting /> */}
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
+      {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" /> */}
+      {/* <Sidebar /> */}
+      <StatefulGreeting greeting="I'm a stateful class component!"/>
     </div>
   );
 }
