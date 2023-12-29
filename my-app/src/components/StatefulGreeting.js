652cd50800beb19500954d267b740bf515755028
Juan Velasquez <juanovelasquez10@gmail.com>
2023-12-29 12:43:03 +0000

    Add constructor to stateful component

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,8 +1,24 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        // this always needs to be added
+        super(props);
+
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",            
+        };
+    };
+
     render () {
-        return <h1>{this.props.greeting}</h1>
+        return (
+            <div>
+                <h1>{this.state.introduction}</h1>
+                <button>{this.state.buttonText}</button>
+            </div>
+        )
     }
 }
 
