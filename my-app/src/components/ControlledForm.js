7196512765a78b736d0e8c3b98f13ef246ccf0ba
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-04 17:29:49 +0000

    Create a form completly manage by react (controlled)

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -7,41 +7,86 @@ export class ControlledForm extends Component {
     
         // Here we start the variable name 
         // and we assign it to an empty string
+        // for the caterogy one, as it is a selection
+        // we put the default as website
         this.state = {
-            name: ''
+            name: '',
+            category: 'website',
+            comments: '',
         }
     }
     
-    // this event handler must need an event
-    // as there is something going on in the input file
+    // event handler for react to capture the input name
     handleNameChange = (event) => {
         this.setState({
-            // this basically updates the input
-            // event is the typing, target is what it needs to do
-            // value is what it needs to update
             name: event.target.value
         })
+    }
+
+    // event handler for react to capture the category selection
+    handleCategoryChange = (event) => {
+        this.setState({
+            category: event.target.value
+        })
+    }
+
+    // event handler for react to capture the text comments
+    handleCommentsChange = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
+
+    // event handler to prevent standard form behavior
+    // The console log will display the information inputed in json <3
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
     }
 
     render() {
         return (
             <div>
-                <form>
-                    {/* standard input label
-                    just note that the for word is reserved in JS
-                    so we need to use the htmlFor */}
-                    <label htmlFor="id-name">Your Name:</label>
-                    {/* this is the text field, the value is the name
-                    then the event listener is the onChange that calls
-                    the method. Then the standard type, name and id */}
-                    <input
-                        value={this.state.name} 
-                        onChange={this.handleNameChange}
-                        id='id-name' 
-                        type="text" 
-                        name='name'
-                    />
-                    {/* standard submit button */}
+                <h2>Please fill out the form</h2>
+                {/* add onSubmit event handler to form */}
+                <form onSubmit={this.handleSubmit}> 
+
+                    {/* for the others is just adding the value
+                    with the correct variable initiated in the constructor
+                    and calling the event handler */}
+                    <div>
+                        <label htmlFor="id-name">Your Name:</label>
+                        <input
+                            value={this.state.name} 
+                            onChange={this.handleNameChange}
+                            id='id-name' 
+                            type="text" 
+                            name='name'
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="id-category">Query category:</label>
+                        <select 
+                            id='id-category' 
+                            name='category'
+                            value={this.state.category} 
+                            onChange={this.handleCategoryChange}
+                        >
+                            <option value='website'>Website issue</option>
+                            <option value='order'>Order issue</option>
+                            <option value='general'>General enquiry</option>
+
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="id-category">Comments:</label>
+                        <textarea 
+                            id='id-comments' 
+                            value={this.state.comments} 
+                            onChange={this.handleCommentsChange}
+                            name='comments'
+                        />
+                    </div>
                     <input type="submit" value="Submit" />
                 </form>
             </div>
