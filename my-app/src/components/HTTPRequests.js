0764770a7d62b8f5066510b84cab5064b52fd031
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-05 13:38:40 +0000

    Improve the get functionality by checking for the data format and catching potential errors

--- my-app/src/components/HTTPRequests.js
+++ my-app/src/components/HTTPRequests.js
@@ -8,29 +8,58 @@ export class HTTPRequests extends Component {
         // set the initial state of posts to an empty array
         this.state = {
             posts: [],
+            error: null,
         }
     }
-
-    // As the trigger to getting the API is mountin this component
     
     componentDidMount() {
-        // makes the API call
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        // handels the response
+        // here we added more numbers in the path to access a specific post
+        // or a post that does not exist
+        axios.get('https://jsonplaceholder.typicode.com/posts/909090')
         .then(response => {
-            // assings the response data to the posts state
+            // when there where all the posts, it is for sure an array
+            // however, if is not an array we need to check it
+            // the if statement checks if posts is an array, 
+            // if it is it returns the response data, if is not, it 
+            // returns the response data within an array so it can be 
+            // iterted in render 
             this.setState({
-                posts: response.data,
+                posts: Array.isArray(response.data)
+                ? response.data
+                : [response.data]
             })
-            console.log(response)
+        })
+        // this is to get the error
+        // and just change the state defiend in the constructor
+        .catch(error => {
+            this.setState({
+                error: error.message,
+            })
         })
     }
 
     render() {
+        const posts = this.state.posts;
         return (
             <div>
                 <h2>Posts: </h2>
-                {JSON.stringify(this.state.posts)}
+                {
+                    posts.length ? (
+                        posts.map(post => (
+                            <div key={post.id}>
+                                <h2>{post.id}. {post.title}</h2>
+                                <h4>By user ID: {post.userId}</h4>
+                                <p>{post.body}</p>
+                                <hr />
+                            </div>
+                        ))
+                    ) : (
+                        this.state.error 
+                        ? <p>{this.state.error}</p>
+                        :
+                        <h4>Loading posts ...</h4>
+                    )
+                }
             </div>
         )
     }
