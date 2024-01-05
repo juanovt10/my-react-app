e1feba986a621d6bd50d873867f81f5dae525bc4
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-05 09:42:28 +0000

    Amend useEffect hook to be triggred only when specific change of states occur rather than being triggered with component rendering

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,16 +2,33 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
-    // useEffect is triggered every time the component is rendered
+
+    // here, for the useEffect to not be triggered 
+    // each time the component is rerendered we add the 
+    // second parameter, in this case [count]
     useEffect(() => {
+        console.log('count 1 effect')
         document.title = count;
-    });
+    }, [count]);
+
+    const [count2, setCount2] = useState(0);
+
+    useEffect(() => {
+        console.log('count 2 effect')
+        document.title = count2;
+    }, [count2]);
+
     return (
         <div>
             <button onClick={
                 () => setCount(count => count + 1)
             }>
                 Increment count ({count})
+            </button>
+            <button onClick={
+                () => setCount2(count2 => count2 + 1)
+            }>
+                Increment count ({count2})
             </button>
         </div>
     )
